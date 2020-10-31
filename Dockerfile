6c29c1767781135b930df2776a67d2ab586ade79
PMJ <pmj@pmj.hu>
2020-10-31 17:17:32 +0100

    Remove tzdata, use UNIX timestamps

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine:3.12
-RUN apk add --no-cache ffmpeg tzdata
+RUN apk add --no-cache ffmpeg
 COPY docker-entrypoint.sh /docker-enrrypoint.sh
 VOLUME /frames
 ENTRYPOINT ["/docker-enrrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,4 +10,4 @@ if [ -z $DEBUG ]; then
     LOGLEVEL="-loglevel panic"
 fi
 
-ffmpeg $LOGLEVEL -rtsp_transport tcp -i $SRC -vf fps=1 -strftime 1 "/frames/%Y-%m-%d_%H-%M-%S.png"
+ffmpeg $LOGLEVEL -rtsp_transport tcp -i $SRC -vf fps=1 -strftime 1 "/frames/%s.png"
